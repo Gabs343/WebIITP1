623df17c93b0ba49b5cc38dc4227434df5848e4e
Gabs343 <gabriellopezp343@gmail.com>
2020-11-01 15:54:59 -0300

    Agregando script faltante

--- footer.php
+++ footer.php
@@ -33,7 +33,7 @@ Footer
 </div>
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="assets/js/jquery.js"></script>
-
+<script src="assets/js/bootstrap.min.js"></script>
 <script src="assets/js/jquery.easing-1.3.min.js"></script>
 <script src="assets/js/jquery.scrollTo-1.4.3.1-min.js"></script>
 <script src="assets/js/shop.js"></script>
